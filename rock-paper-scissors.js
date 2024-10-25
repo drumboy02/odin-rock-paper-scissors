3e32407a6bbceb6144542480459fc6dc77d13233
drumboy02 <153551032+drumboy02@users.noreply.github.com>
2024-10-24 22:48:48 -0400

    fixed Choice functions to return string instead of number

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,15 +1,30 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
-    console.log("getComputerChoice: ", choice);
+    if (choice === 1) {
+        choice = "rock";
+    } else if (choice === 2) {
+        choice = "paper";
+    } else if (choice === 3) {
+        choice = "scissors";
+    }
+    console.log("getComputerChoice:", choice);
 }
 
 function getHumanChoice() {
-    let choice = +prompt("Choose 1, 2 or 3");
-    console.log("getHumanChoice: ", choice);
+    let choice = prompt("Choose rock, paper, or scissors").toLowerCase();
+    if (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+        getHumanChoice();
+    } else {
+        console.log("getHumanChoice:", choice);
+    }
 }
-
-getComputerChoice();
-getHumanChoice();
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    getHumanChoice();
+    getComputerChoice();
+}
+
+playRound();
