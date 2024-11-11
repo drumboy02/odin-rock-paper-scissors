fc3b2e0c599e60990a5185abf5b76ba4ecb07fff
drumboy02 <153551032+drumboy02@users.noreply.github.com>
2024-11-10 19:08:06 -0500

    changed the order of some console.log statements

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -29,46 +29,46 @@ function playGame() {
             case "rock paper":
             case "paper rock":
                 if (humanChoice === "paper") {
-                    console.log("You win! Paper beats Rock");
                     console.log("");
+                    console.log("You win! Paper beats Rock");                    
                     return humanScore += 1;
                 } else {
-                    console.log("You lose! Paper beats Rock");
                     console.log("");
+                    console.log("You lose! Paper beats Rock");                    
                     return computerScore += 1;
                 }
             case "rock scissors":
             case "scissors rock":
                 if (humanChoice === "rock") {
-                    console.log("You win! Rock beats Scissors");
                     console.log("");
+                    console.log("You win! Rock beats Scissors");                    
                     return humanScore += 1;
                 } else {
-                    console.log("You lose! Rock beats Scissors");
                     console.log("");
+                    console.log("You lose! Rock beats Scissors");                    
                     return computerScore += 1;
                 }
             case "paper scissors":
             case "scissors paper":
                 if (humanChoice === "scissors") {
-                    console.log("You win! Scissors beats Paper");
                     console.log("");
+                    console.log("You win! Scissors beats Paper");                    
                     return humanScore += 1;
                 } else {
-                    console.log("You lose! Scissors beats Paper");
                     console.log("");
+                    console.log("You lose! Scissors beats Paper");                    
                     return computerScore += 1;
                 }
             case "rock rock":
             case "paper paper":
             case "scissors scissors":
-                console.log("It's a tie. Choose again")
                 console.log("");
+                console.log("It's a tie. Choose again")                
                 playRound(getHumanChoice(), getComputerChoice());
                 break;
             default:
-                console.log("Choose Rock, Paper, or Scissors")
                 console.log("");
+                console.log("Choose Rock, Paper, or Scissors")                
                 playRound(getHumanChoice(), getComputerChoice());
                 break;
         }
